6870ce12bb1b31f2760e38ca82d54c89880592f3
Ehsan Hadid <Ehsan.hadid3242@gmail.com>
2023-02-21 19:47:07 +0430

    convert app from js to ts

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import PhoneVerification from "./components/phone-verification";
+
+function App() {
+  return (
+    <div className="App">
+      <PhoneVerification></PhoneVerification>
+    </div>
+  );
+}
+
+export default App;
